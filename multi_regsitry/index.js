14989f9536714110d6f67b8e4809e8884e3ecd63
kursivee <kursivee.dev@gmail.com>
2020-06-03 14:16:59 -0400

    [multi_registry] Initial commit with 2 app registries

---
+++ multi_regsitry/index.js
@@ -0,0 +1,10 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import App1 from './App1';
+import App2 from './App2'
+
+AppRegistry.registerComponent("App1", () => App1);
+AppRegistry.registerComponent("App2", () => App2);
