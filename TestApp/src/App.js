fb4763c05bf6221f179b07e3ea49832942d6a6d3
kursivee <kursivee.dev@gmail.com>
2020-06-13 07:24:42 -0400

    [TestApp] Setup initial view

---
+++ TestApp/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { SafeAreaView } from 'react-native';
+import HomePage from './component/page/home.page'
+
+const App = () => {
+  return (
+    <>
+      <SafeAreaView>
+          <HomePage />
+      </SafeAreaView>
+    </>
+  );
+};
+
+export default App;
---
+++ TestApp/src/component/page/home.page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import HomeTemplate from '../template/home.template'
+
+interface Props {
+
+}
+
+const HomePage: React.FC<Props> = () => {
+    return(
+        <HomeTemplate />
+    )
+}
+
+export default HomePage
